534cb77da0241c4d5db7f418a31d49445b62cfb8
Duong Pham Minh Dung <94067220+duongphamminhdung@users.noreply.github.com>
2023-03-15 20:07:26 +0700

    15/3 commit

--- handwriting_nums recognize/training_model.py
+++ handwriting_nums recognize/training_model.py
@@ -1,21 +1,26 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+
 
 #import mnist dataset
-data = pd.read_csv("/Users/duongphamminhdung/Documents/GitHub/solving basic mcln problems/handwriting_nums recognize/data/mnist_test.csv", sep=',')
+data = pd.read_csv("/Users/duongphamminhdung/Documents/GitHub/DATA SETS/mnist/mnist_test.csv", sep=',')
 mnist = data.values
 label = mnist[:, 0]
 digits = mnist[:, 1:]
 
 X_train = digits[:7000]
 Y_train = label[:7000]
-X_test = digits[-1]
-Y_test = label[-1]
-
+X_test = digits[-10:]
+Y_test = label[-10:]
+def normalize(X):
+    X = X / 255
+    return X
 def find_target(Y):
     """
     tạo one-hot vector 
     """
+    Y = Y.reshape(len(Y))
     s = np.zeros((len(Y), 10))
     s[np.arange(Y.size), Y] = 1
     return s
@@ -25,67 +30,77 @@ def softmax(Z):
     Z.shape = (N_class, 1)
     a_i = e^(z_i)/sum(e^Z)
     """
-    ez = np.exp(Z-np.max(Z, axis = 1, keepdims = True)) #trừ đi mỗi số giá trị lớn nhất của hàng đó
-    A = ez/np.sum(ez) #dãy tỉ lệ rơi vào class 
+    ez = np.exp(Z) 
+    A = ez / np.sum(ez, axis = 1)[:, None] #dãy tỉ lệ rơi vào class 
     return A
 def d_softmax(x):
     ez=np.exp(x-x.max())
     return ez/np.sum(ez,axis=0)*(1-ez/np.sum(ez,axis=0))
 def activate_func(Z):
     #activate
-    Z[Z <= 0] = 0
+    Z = 1/(np.exp(-Z)+1)    
     return Z
-def calc(X, Y, W1, W2):
+def d_sigmoid(x):
+    return (np.exp(-x))/((np.exp(-x)+1)**2)
+def calc_loss(x, y, W1, W2):
     """ 
     X, Y : 128x784
     W1: 784x128
     W2: 128x10
     """
-    targets = find_target(Y)        #target     128x10
-    x_1=X.dot(W1)                   #x_1        128x128
-    x_activate=activate_func(x_1)   #x_activate  128x128
-    x_2=(x_activate).dot(W2)        #x_2        128x10
-    cost=softmax(x_2)               #cost       128x10
+    targets = find_target(y)        #target     batchsizex10
+    x_1=x.dot(W1)                   #x_1        batchsizex128
+    x_activate=activate_func(x_1)   #x_activate batchsizex128
+    x_2=(x_activate).dot(W2)        #x_2        batchsizex10
+    cost=softmax(x_2)               #cost       batchsizex10
+    loss = (-np.mean(targets*np.log(cost)))*len(y) #batchsize, ()
+    # print(x_2)
+    return cost, loss
+def calc(x, y, W1, W2):
+    h = 1e-3
+    cost, loss = calc_loss(x, y, W1, W2)
+    cost_, loss_ = calc_loss(x-h, y, W1, W2)
+    d_loss = 1/2 * (loss - loss_)**2
+    update_1=(x).T.dot(d_loss)                  #update1    784xbatchsize
+    update_2=((activate_func(x.dot(W1))).dot(W2)).T.dot(d_loss )               #update2    10xbatchsize
+    
+    return cost, loss, update_1, update_2 
 
-    loss = (np.sum(-targets*np.log(cost), axis = 1)/len(y))[..., None] #128, 1
-    update_2=x_activate.T@loss
 
-
-    loss=((W2).dot(loss.T)).T*(x_1)
-    update_1=X.T@loss
-
-    return cost,update_1,update_2 
-
-W1 = np.random.randn(784, 128)
-W2 = np.random.randn(128, 10)
-
-epoch = 1000
+epoch = 10000
 acc = []
 losses = []
-lr = 0.005
-batch = 128
+lr = 0.01
+batch = 256
+i = 0
+W1 = np.random.randn(784, batch)
+W2 = np.random.randn(batch, 10)
 
-for i in range(epoch):
-    #randomize and create batches
+while i < epoch:
+    b = 1e-3
+    i += 1
     sample=np.random.randint(0,X_train.shape[0],size=(batch))
     x=X_train[sample]
     y=Y_train[sample][:, None]
-
-    out,update_1,update_2= calc(x, y, W1, W2)   
+    x = normalize(x)
+    out,loss, update_1,update_2= calc(x, y, W1, W2)   
+    
     category=np.argmax(out,axis=1)
-    
-    accuracy=(category==y).mean()
+    accuracy=(category[:, None]==y).mean()
     acc.append(accuracy.item())
-    
-    loss=((category-y)**2).mean()
-    losses.append(loss.item())
+    # import ipdb; ipdb.set_trace()
+    losses.append(loss)
     
     #SGD 
-    W1=W1-lr*update_1
-    W2=W2-lr*update_2
-
+    W1=W1-lr*(update_1 + b)
+    W2=W2-lr*(update_2.T+ b)
+    # import ipdb; ipdb.set_trace()
     if i%100 == 0:
         print("epoch", i, "accuracy:", acc[-1])
-test=np.argmax(softmax((X_test.dot(W1)).dot(W2)),axis=1)
-accuracy=(test==Y_test).mean().item()
-print(f'Test accuracy = {accuracy:.4f}')
+        print("epoch", i, "loss:", losses[-1])
+# test = softmax((activate_func(X_test.dot(W1))).dot(W2))
+# accuracy=(test==Y_test).mean().item()
+# print(f'Test accuracy = {accuracy:.4f}')
+plt.grid()
+plt.plot([i for i in range(1000)], losses)
+plt.show()
